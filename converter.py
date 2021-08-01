8a5da34b7fb7711621e30ccf8ef25d96ac44dd8b
Jonathan Gregg <jpgregg@utexas.edu>
2021-07-31 18:15:28 -0700

    remove garbage comments and start to clean file.

--- converter.py
+++ converter.py
@@ -1,64 +1,20 @@
+#!/usr/bin/env python
 '''
-Purpose: To recursively look through folder and child folders to change all contained Webp files to PNG files.
-    These will not change the original, but will create a folder titled "PNGs" that contains all of the new images.
-
-Need to provide code to link the webp to png converter.
-
-Pseudocode:
-converter = "C\\blah\\blah"         this makes a variable that saves the location of the converter.
-
-
+Copies every .webp file to .png from "start_directory" on down.
+**This requires libwebp converter from URL: https://developers.google.com/speed/webp/download
 '''
 
 import os
 import subprocess
 
-#os.chdir("C:\\Users\\gregg\\OneDrive\\Desktop")
-
-#print(os.getcwd())
-
-#print(os.listdir())
-
-#os.makedirs('os_test\\os_test_deeper')
-#os.removedirs('os_test2\\os_test_deeper')
-
 converter_location = 'C:\\Users\\gregg\\OneDrive\\Desktop\\VSCode Stuff\\Webp_to_Png\\libwebp-1.2.0-windows-x64\\bin\\dwebp'
 start_directory = 'C:\\Users\\gregg\\OneDrive\\Desktop\\testFolder'
 
 for dirpath, dirnames, filenames in os.walk(start_directory):
-    #print('Current Path:', dirpath)
-    #print('Directories:', dirnames)
-    #print('Files:', filenames)
-    #print()
-
     for file in filenames:
-        if file.endswith(".webp"):
-            print("converting...")
-            ##webp_file_location = dirpath + '\\' + file + ' '
-            ##png_file = '-o ' + dirpath + '\\' + file.replace(".webp", ".png")
-            
+        if file.endswith(".webp"):           
             webp_file_location = dirpath + '\\' + file
             png_file = dirpath + '\\' + file.replace(".webp", ".png")
+            
             file = f'{converter_location} {webp_file_location} -o {png_file}'
-
-            ##file = converter_location + webp_file_location + png_file 
-            
-            
-            #file = 'C:\\Users\\gregg\\OneDrive\\Desktop\\VSCode Stuff\\Webp_to_Png\\libwebp-1.2.0-windows-x64\\bin\\dwebp C:\\Users\\gregg\\OneDrive\\Desktop\\testFolder\\level1Folder1\\image1.webp -o C:\\Users\\gregg\\OneDrive\\Desktop\\testFolder\\level1Folder1\\image1.png'
             subprocess.call(file)
-            #print(file)
-
-'''
-the for loop is now looping through every file and every directory.
-in each directory, examine every file.
-    for every file that ends in .webp:
-        call the converter app on this file and paste result to directory.
-
-
-dwebp test2.webp -o test2.png
-
-dwebp is the decoding program that takes webp files as input
-test2.webp was the name of the webp file
--o specifies the output name, I THINK.
-test2.png is output file name.
-'''
